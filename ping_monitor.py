53c3a08ab023e0bdbffa5816bd82034c97edf473
松永健汰 <matsunagaken@MacBook-Air-4.local>
2019-05-07 19:36:44 +0900

    FirstCommit

---
+++ ping_monitor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from flask import *
+from datetime import datetime
+
+app = Flask(__name__, static_folder="static")
+import subprocess
+
+
+f = open('ip_table.txt')
+a = f.readlines()
+
+ip = []
+name = []
+
+for line in a:
+    l = line.split()
+    ip.append(l[0])
+    name.append(l[1])
+
+
+ping = []
+stable = []
+
+@app.route('/ping_monitor')
+def ping_monitor():
+    for i in range(len(ip)):
+        p = subprocess.call(['ping','-c','1','-W','1',ip[i]])
+        ping.append(p)
+        
+        if p == 0:
+            stable.append('Connection OK!!!!!')
+        else :
+            stable.append('No Connection!!!!!!')
+            
+            return render_template('index.html',PING = zip(ip, name, stable))
+        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True, port=9876)
+                
+                                                                            
