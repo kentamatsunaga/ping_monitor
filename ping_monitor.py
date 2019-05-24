a6a1180f00d9c2bb4d1abac3e63fd52a3e3d3f0b
KazukiShiotani <shiotani@a.phys.nagoya-u.ac.jp>
2019-05-24 09:57:44 +0900

    add_lastupdate

--- ping_monitor.py
+++ ping_monitor.py
@@ -25,14 +25,16 @@ stable = []
 
 @app.route('/')
 def ping_monitor():
+    now = datetime.utcnow()
+    date = now.strftime("%Y-%m-%d %H:%M:%S")
     for i in range(len(ip)):
         p = subprocess.call(['ping','-c','1','-W','1',ip[i]])
         ping.append(p)
         if p == 0:
             stable.append('Connection OK!!!!!')
         else :
-            stable.append('No Connection!!!!!!')            
-    return render_template('index.html',PING = zip(ip, name, stable))
+            stable.append('No Connection!!!!!!')
+    return render_template('index.html',PING = zip(ip, name, stable), date = date)
         
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=9888)
